8e5b7579d37ae5047cea95c8f447344d205d99cf
Ulysses Zhan <ulysseszhan@gmail.com>
2023-07-21 22:47:53 -0700

    fixed another compatibility bug on old safari

--- js/utils/Patches.js
+++ js/utils/Patches.js
@@ -1,10 +1,24 @@
 Sunniesnow.Patches = {
 	apply() {
 		this.patchHasOwn();
+		this.patchFindLastIndex();
 	},
 
 	// Object.hasOwn is supported on Safari only since 15.4.
 	patchHasOwn() {
 		Object.hasOwn ||= (object, key) => Object.prototype.hasOwnProperty.call(object, key);
+	},
+
+	// Array.prototype.findLastIndex is supported on Safari only since 15.4.
+	patchFindLastIndex() {
+		Array.prototype.findLastIndex ||= function(callbackFn, thisArg) {
+			thisArg ??= globalThis;
+			for (let i = this.length - 1; i >= 0; i--) {
+				if (callbackFn.call(thisArg, this[i], i, this)) {
+					return i;
+				}
+			}
+			return -1;
+		}
 	}
 };
