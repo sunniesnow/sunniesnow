13e3c06cd7e812d173bcf1095e24b2bea63d525b
Ulysses Zhan <ulysseszhan@gmail.com>
2023-09-18 15:39:09 -0700

    fix big text wrong clipped fix big text not displayed in mobile edge in some cases (but it is still reproduceable in edge cases) big text now abort when text is longer than 20

--- js/ui-event/UiBigText.js
+++ js/ui-event/UiBigText.js
@@ -14,40 +14,53 @@ Sunniesnow.UiBigText = class UiBigText extends Sunniesnow.UiBgPattern {
 				Sunniesnow.Utils.warn(`Failed to load font for big texts: ${e.message ?? e}`, e);
 			}
 		}
-		this.fontSize = Sunniesnow.Config.radius * 10 * Sunniesnow.Config.scale();
+		this.fontSize = Sunniesnow.Config.radius * 5 * Sunniesnow.Config.scale();
 		this.maxWidth = Sunniesnow.Config.minWidth * Sunniesnow.Config.scale();
 	}
 
 	populate() {
 		super.populate();
+		if (this.event.text.length > 20) {
+			Sunniesnow.Utils.warn(`Text "${this.event.text}" is too long`);
+			this.aborted = true;
+			return;
+		}
 		this.text = new PIXI.Text(this.event.text, {
 			fontFamily: 'HanWangShinSuMedium-Regular,YujiBoku-Regular,Arial',
 			fontSize: this.constructor.fontSize,
 			fill: 0xffffff,
-			align: 'center'
+			align: 'center',
+			padding: this.constructor.fontSize / 2
 		});
 		this.text.anchor = new PIXI.ObservablePoint(null, null, 0.5, 0.5);
-		if (this.text.width > this.constructor.maxWidth) {
-			this.scale.set(this.constructor.maxWidth / this.text.width);
-		}
+		this.scale.set(Math.min(2, this.constructor.maxWidth / this.text.width));
 		this.addChild(this.text);
 		this.alpha = 0.8;
 	}
 
 	updateFadingIn(progress, relativeTime) {
 		super.updateFadingIn(progress, relativeTime);
+		if (this.aborted) {
+			return;
+		}
 		this.text.alpha = progress;
 		this.text.scale.set(progress);
 	}
 
 	updateHolding(progress, relativeTime) {
 		super.updateHolding(progress, relativeTime);
+		if (this.aborted) {
+			return;
+		}
 		this.text.alpha = 1;
 		this.text.scale.set(1);
 	}
 
 	updateFadingOut(progress, relativeTime) {
 		super.updateFadingOut(progress, relativeTime);
+		if (this.aborted) {
+			return;
+		}
 		this.text.alpha = 1 - progress;
 	}
 };
