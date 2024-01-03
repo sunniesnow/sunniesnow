f1065b8ae0669a054670c02d69aecd8bceddf000
Ulysses Zhan <ulysseszhan@gmail.com>
2024-01-03 14:51:03 -0800

    adapt pixi patches to pixi 7.3

--- js/utils/Assets.js
+++ js/utils/Assets.js
@@ -22,23 +22,25 @@ Sunniesnow.Assets = {
 
 	async loadFont(url, family) {
 		if (Sunniesnow.Utils.isBrowser()) {
-			const result = await PIXI.Assets.load({ src: url, loadParser: 'loadWebFont', data: { family } });
+			const result = await PIXI.Assets.load({src: url, loadParser: 'loadWebFont', data: {family}});
 			if (!(result instanceof FontFace)) {
 				throw new Error('Failed to load font');
 			}
 		} else {
+			// https://github.com/pixijs/node/issues/5
 			const path = require('path');
 			const fs = require('fs');
 			const dest = path.join(Sunniesnow.record.tempDir, path.basename(url));
 			const data = await Sunniesnow.Utils.strictFetch(url).then(res => res.arrayBuffer());
 			fs.writeFileSync(dest, Buffer.from(data));
-			return await PIXI.Assets.load({src: dest, data: {family}});
+			return await PIXI.Assets.load({src: dest, loadParser: 'loadNodeFont', data: {family}});
 		}
 	},
 
 	async audioDecode(arrayBuffer, context) {
 		const audioBuffer = await audioDecode(arrayBuffer);
 		const result = context.createBuffer(audioBuffer.numberOfChannels, audioBuffer.length, audioBuffer.sampleRate);
+		// https://github.com/audiojs/audio-decode/pull/35#issuecomment-1656137481
 		for (let i = 0; i < audioBuffer.numberOfChannels; i++) {
 			result.copyToChannel(audioBuffer.getChannelData(i), i);
 		}
--- js/utils/PixiPatches.js
+++ js/utils/PixiPatches.js
@@ -1,18 +1,21 @@
 Sunniesnow.PixiPatches = {
 	apply() {
-		this.patchHasProtocol();
+		// this.patchHasProtocol(); // fixed in PIXI 7.3
 		if (!Sunniesnow.Utils.isBrowser()) {
 			this.patchLoadSvg();
 			this.patchNodeLoaderParsers();
 		}
 	},
 
+	// https://github.com/pixijs/pixijs/issues/9568#issuecomment-1653126302
+	// fixed in https://github.com/pixijs/pixijs/commit/d1f23cfe62bf5004533ec4ac565b1945aa6ccdb3
 	patchHasProtocol() {
 		PIXI.utils.path.hasProtocol = function (path) {
 			return (/^[^/:]+:/).test(this.toPosix(path));
 		};
 	},
 
+	// https://github.com/pixijs/node/issues/4
 	patchLoadSvg() {
 		let src = PIXI.SVGResource.prototype._loadSvg.toString().match(/\{(.*)\}/s)[1];
 		src = src.replace(/tempImage\.src\s*=\s*this\.svg[,;]/, '');
@@ -23,6 +26,7 @@ Sunniesnow.PixiPatches = {
 		)(require('canvas').Image, PIXI.BaseImageResource, PIXI.utils.uid);
 	},
 
+	// https://github.com/pixijs/node/pull/13
 	patchNodeLoaderParsers() {
 		PIXI.loadNodeTexture.name = 'loadNodeTexture';
 		PIXI.loadNodeFont.name = 'loadNodeFont';
