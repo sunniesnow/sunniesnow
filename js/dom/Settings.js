sunniesnow/sunniesnow

commit 79e452c7f108f60ddeb262bab053e5dae5d76404

add options hide-circles and circle-moves-with-note

js/dom/Settings.js
@@ -179,6 +179,8 @@ Sunniesnow.Settings = {
 			scrollDistance: this.readValue('scroll-distance'),
 			touchEffects: this.readCheckbox('touch-effects'),
 			reverseNoteOrder: this.readCheckbox('reverse-note-order'),
+			hideCircles: this.readCheckbox('hide-circles'),
+			circleMovesWithNote: this.readCheckbox('circle-moves-with-note'),
 			hideTipPoints: this.readCheckbox('hide-tip-points'),
 			hideNotes: this.readCheckbox('hide-notes'),
 			hideBgNotes: this.readCheckbox('hide-bg-notes'),
@@ -339,6 +341,8 @@ Sunniesnow.Settings = {
 		this.writeValue('scroll-distance', d('scrollDistance'));
 		this.writeCheckbox('touch-effects', d('touchEffects'));
 		this.writeCheckbox('reverse-note-order', d('reverseNoteOrder'));
+		this.writeCheckbox('hide-circles', d('hideCircles'));
+		this.writeCheckbox('circle-moves-with-note', d('circleMovesWithNote'));
 		this.writeCheckbox('hide-tip-points', d('hideTipPoints'));
 		this.writeCheckbox('hide-notes', d('hideNotes'));
 		this.writeCheckbox('hide-bg-notes', d('hideBgNotes'));

index.html
@@ -350,6 +350,30 @@ description: Sunniesnow is a web rhythm game
 	</fieldset>
 
 	<fieldset>
+		<legend id="hide-category"></legend>
+
+		<div>
+			<input type="checkbox" id="hide-tip-points">
+		</div>
+		<div>
+			<input type="checkbox" id="hide-notes">
+		</div>
+		<div>
+			<input type="checkbox" id="hide-circles">
+		</div>
+		<div>
+			<input type="checkbox" id="hide-bg-notes">
+		</div>
+		<div>
+			<input type="checkbox" id="hide-fx">
+		</div>
+		<div>
+			<input type="checkbox" id="hide-bg-pattern">
+		</div>
+
+	</fieldset>
+	
+	<fieldset>
 		<legend id="misc-visual-category"></legend>
 
 		<div>
@@ -361,15 +385,10 @@ description: Sunniesnow is a web rhythm game
 		</div>
 
 		<div>
-			<span id="hide-introducer"></span>
-			<input type="checkbox" id="hide-tip-points">
-			<input type="checkbox" id="hide-notes">
-			<input type="checkbox" id="hide-bg-notes">
-			<input type="checkbox" id="hide-fx">
-			<input type="checkbox" id="hide-bg-pattern">
+			<input type="checkbox" id="circle-moves-with-note">
 		</div>
-
 	</fieldset>
+
 </details>
 
 <details>

js/chart/Event.js
@@ -87,8 +87,8 @@ Sunniesnow.Event = class Event {
 		return this.endTime() + this.uiClass().fadingOutDuration(this);
 	}
 
-	newUiEvent(fxBoard) {
-		return new (this.uiClass())(this, fxBoard);
+	newUiEvent() {
+		return new (this.uiClass())(this);
 	}
 
 	checkProperties() {

js/dom/I18n.js
@@ -57,8 +57,8 @@ Sunniesnow.I18n = {
 			simultaneityHintsIntroducer: 'Connect these kinds of simultaneous notes:',
 			fxOptionsCategory: 'FX options',
 			scrollVisualCategory: 'Scroll mode',
+			hideCategory: 'Hide',
 			miscVisualCategory: 'Miscellaneous',
-			hideIntroducer: 'Hide:',
 
 			audioSettings: 'Audio settings',
 			seCategory: 'SE (note hit sound effects)',
@@ -187,8 +187,8 @@ Sunniesnow.I18n = {
 			simultaneityHintsIntroducer: '连接这些音符的多押:',
 			fxOptionsCategory: 'FX 选项',
 			scrollVisualCategory: '下落式',
+			hideCategory: '隐藏',
 			miscVisualCategory: '杂项',
-			hideIntroducer: '隐藏:',
 
 			audioSettings: '音频设置',
 			seCategory: 'SE (打击音效)',
@@ -331,13 +331,15 @@ Sunniesnow.I18n = {
 			alwaysUpdateFx: ' Always update FX even when pausing',
 			scrollJudgementLine: 'Judgement line position: ',
 			scrollDistance: 'Scroll distance: ',
-			touchEffects: ' Touch effects',
-			reverseNoteOrder: ' Reverse the display order of notes',
 			hideTipPoints: ' Tip points',
 			hideNotes: ' Notes',
+			hideCircles: ' Circles',
 			hideBgNotes: ' Background notes',
 			hideFx: ' FX',
 			hideBgPattern: ' Background pattern',
+			touchEffects: ' Touch effects',
+			reverseNoteOrder: ' Reverse the display order of notes',
+			circleMovesWithNote: ' Circles move with notes',
 
 			seDefaultRadio: ' Default',
 			seOnlineRadio: '',
@@ -479,13 +481,15 @@ Sunniesnow.I18n = {
 			alwaysUpdateFx: ' 暂停时也更新 FX',
 			scrollJudgementLine: '判定线位置: ',
 			scrollDistance: '下落距离: ',
-			touchEffects: ' 触摸效果',
-			reverseNoteOrder: ' 音符显示顺序反转',
 			hideTipPoints: ' 游标',
 			hideNotes: ' 音符',
+			hideCircles: ' 缩圈',
 			hideBgNotes: ' 墨点',
 			hideFx: ' FX',
 			hideBgPattern: ' 背景图案',
+			touchEffects: ' 触摸效果',
+			reverseNoteOrder: ' 音符显示顺序反转',
+			circleMovesWithNote: ' 缩圈随音符移动',
 
 			seDefaultRadio: ' 默认',
 			seOnlineRadio: '',

js/fx/FxNote.js
@@ -9,6 +9,9 @@ Sunniesnow.FxNote = class FxNote extends PIXI.Container {
 		this.state = 'present'; // present -> finished
 		this.levelNote = levelNote;
 		[this.x, this.y] = [this.front.x, this.front.y] = Sunniesnow.Config.chartMapping(levelNote.event.x, levelNote.event.y);
+		if (Sunniesnow.game.settings.scroll) {
+			this.y = this.front.y = Sunniesnow.Config.SCROLL_END_Y;
+		}
 		this.judgement = levelNote.judgement || levelNote.highestJudgement;
 		this.earlyLate = levelNote.earlyLate;
 		this.populate();

js/scene/SceneGame.js
@@ -28,10 +28,12 @@ Sunniesnow.SceneGame = class SceneGame extends Sunniesnow.Scene {
 		}
 		if (!Sunniesnow.game.settings.hideNotes) {
 			this.doubleLinesBoard = new Sunniesnow.DoubleLinesBoard();
-			this.uiNotesBoard = new Sunniesnow.UiNotesBoard();
 		}
 		if (Sunniesnow.game.settings.scroll) {
 			this.judgementLine = new Sunniesnow.JudgementLine();
+		}
+		if (!Sunniesnow.game.settings.hideNotes || !Sunniesnow.game.settings.hideCircles) {
+			this.uiNotesBoard = new Sunniesnow.UiNotesBoard();
 		}
 		this.background.addTo(this);
 		this.progressBar.addTo(this);
@@ -74,14 +76,14 @@ Sunniesnow.SceneGame = class SceneGame extends Sunniesnow.Scene {
 		if (Sunniesnow.game.progressAdjustable) {
 			Sunniesnow.ProgressControl.update(delta);
 		}
-		this.updateUiComponents(delta);
-		this.updateBoards(delta);
 		if (!Sunniesnow.Music.pausing) {
 			Sunniesnow.game.level.update();
 			if (Sunniesnow.game.level.finished) {
 				this.gotoResult();
 			}
 		}
+		this.updateUiComponents(delta);
+		this.updateBoards(delta);
 	}
 
 	gotoResult() {

js/ui-event/UiDrag.js
@@ -3,9 +3,6 @@ Sunniesnow.UiDrag = class UiDrag extends Sunniesnow.UiNote {
 	static async load() {
 		this.radius = Sunniesnow.Config.NOTE_RADIUS * 2/3;
 		this.geometry = this.createGeometry();
-		if (Sunniesnow.game.settings.scroll) {
-			return;
-		}
 		this.circleRadius = this.radius * 6;
 		this.circleGeometry = this.createCircleGeometry();
 	}
@@ -36,17 +33,17 @@ Sunniesnow.UiDrag = class UiDrag extends Sunniesnow.UiNote {
 	populate() {
 		super.populate();
 		this.note = new PIXI.Graphics(this.constructor.geometry);
-		if (!Sunniesnow.game.settings.scroll) {
-			this.circle = new PIXI.Graphics(this.constructor.circleGeometry);
-			this.addChild(this.circle);
-		}
 		this.addChild(this.note);
+	}
+
+	populateCircle() {
+		this.circle = new PIXI.Graphics(this.constructor.circleGeometry);
 	}
 
 	updateFadingIn(progress, relativeTime) {
 		super.updateFadingIn(progress, relativeTime);
 		this.note.scale.set(progress);
-		if (Sunniesnow.game.settings.scroll) {
+		if (!this.circle) {
 			return;
 		}
 		this.circle.scale.set(1 - (progress-1)**2);
@@ -56,7 +53,7 @@ Sunniesnow.UiDrag = class UiDrag extends Sunniesnow.UiNote {
 	updateActive(progress, relativeTime) {
 		super.updateActive(progress, relativeTime);
 		this.note.scale.set(1);
-		if (Sunniesnow.game.settings.scroll) {
+		if (!this.circle) {
 			return;
 		}
 		const targetCircleScale = this.constructor.radius / this.constructor.circleRadius;

js/ui-event/UiFlick.js
@@ -3,9 +3,6 @@ Sunniesnow.UiFlick = class UiFlick extends Sunniesnow.UiNote {
 		this.radius = Sunniesnow.Config.NOTE_RADIUS;
 		this.geometry = Sunniesnow.UiTap.createGeometry.call(this, 0xfe6e4e, 0xffffff);
 		this.arrowGeometry = this.createArrowGeometry();
-		if (Sunniesnow.game.settings.scroll) {
-			return;
-		}
 		this.circleRadius = this.radius * 4;
 		this.circleGeometry = Sunniesnow.UiTap.createCircleGeometry.call(this, 0xccfcfc);
 	}
@@ -37,10 +34,6 @@ Sunniesnow.UiFlick = class UiFlick extends Sunniesnow.UiNote {
 		this.arrow = new PIXI.Graphics(Sunniesnow.UiFlick.arrowGeometry);
 		this.arrow.transform.rotation = Sunniesnow.Config.chartMappingAngle(this.event.angle);
 		this.text = Sunniesnow.UiTap.prototype.createText.call(this);
-		if (!Sunniesnow.game.settings.scroll) {
-			this.circle = new PIXI.Graphics(Sunniesnow.UiFlick.circleGeometry);
-			this.addChild(this.circle);
-		}
 		this.note = new PIXI.Container();
 		this.note.addChild(this.noteBody);
 		this.note.addChild(this.text);
@@ -48,12 +41,16 @@ Sunniesnow.UiFlick = class UiFlick extends Sunniesnow.UiNote {
 		this.addChild(this.note);
 	}
 
+	populateCircle() {
+		this.circle = new PIXI.Graphics(Sunniesnow.UiFlick.circleGeometry);
+	}
+
 	updateFadingIn(progress, relativeTime) {
 		super.updateFadingIn(progress, relativeTime);
 		this.note.scale.set(progress);
 		this.arrow.alpha = progress;
 		this.arrow.scale.set(1 + 0.05*Math.cos(relativeTime * 2));
-		if (Sunniesnow.game.settings.scroll) {
+		if (!this.circle) {
 			return;
 		}
 		this.circle.scale.set(1 - (progress - 1) ** 2);
@@ -64,7 +61,7 @@ Sunniesnow.UiFlick = class UiFlick extends Sunniesnow.UiNote {
 		super.updateActive(progress, relativeTime);
 		this.note.scale.set(1);
 		this.arrow.scale.set(1 + 0.05*Math.cos(relativeTime * 5));
-		if (Sunniesnow.game.settings.scroll) {
+		if (!this.circle) {
 			return;
 		}
 		const targetCircleScale = this.constructor.radius / this.constructor.circleRadius;
@@ -81,7 +78,7 @@ Sunniesnow.UiFlick = class UiFlick extends Sunniesnow.UiNote {
 		super.updateFadingOut(progress, relativeTime);
 		Sunniesnow.UiTap.prototype.updateTextFadingOut.call(this, progress);
 		this.noteBody.visible = false;
-		if (!Sunniesnow.game.settings.scroll) {
+		if (this.circle) {
 			this.circle.visible = false;
 		}
 		progress *= 2;

commit 7ebfb4706667b8354f2c7cccb4bc3bd19d332665

fix deselected avatar radio does not deactivate corresponding inputs; fix event info tip does not show (bug introduced by 79e452c)

js/dom/Settings.js
@@ -785,6 +785,9 @@ Sunniesnow.Settings = {
 		this.associateCheckbox('scroll', 'scroll-distance');
 		this.associateCheckbox('vibration-with-music', 'vibration-delay');
 		this.associateRange('hold-vibration-duty-cycle', 'hold-vibration-duty-cycle-value');
+		this.associateRadio('avatar-online-radio', 'avatar-online');
+		this.associateRadio('avatar-upload-radio', 'avatar-upload');
+		this.associateRadio('avatar-gravatar-radio', 'avatar-gravatar');
 	},
 
 	clearDownloadingProgresses() {

js/interaction/EventInfoTip.js
@@ -49,7 +49,7 @@ Sunniesnow.EventInfoTip = {
 		let minDistance = Infinity;
 		let nearest = null;
 		const {canvasX: x, canvasY: y} = touch.start();
-		for (const uiEvent of this.uiNotesBoard.uiEvents) {
+		for (const {uiEvent} of this.uiNotesBoard.uiEvents) {
 			if (uiEvent.state === 'ready' || uiEvent.state === 'finished') {
 				continue;
 			}