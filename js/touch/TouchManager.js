sunniesnow/sunniesnow

commit 36757c0ff25cab8dfad6adffca6725b997bc8e7f

better keydown handling

js/touch/TouchManager.js
@@ -45,7 +45,7 @@ Sunniesnow.TouchManager = {
 	},
 
 	onStart(touch) {
-		document.activeElement.blur(); // see preventKeyEventIfShould
+		document.activeElement.blur();
 		Sunniesnow.game.window.focus(); // In VSCode Simple Browser, this acquires focus lock
 		const existingTouch = this.touches[touch.id];
 		if (existingTouch) {
@@ -54,25 +54,27 @@ Sunniesnow.TouchManager = {
 			return;
 		}
 		this.touches[touch.id] = touch;
-		this.callListeners(this.startListeners, touch)
+		return this.callListeners(this.startListeners, touch);
 	},
 
 	onMove(touch) {
-		this.callListeners(this.moveListeners, touch)
+		const result = this.callListeners(this.moveListeners, touch);
 		touch.needsUpdating = false;
+		return result;
 	},
 
 	onEnd(touch) {
 		touch.finished = true;
-		this.callListeners(this.endListeners, touch)
+		return this.callListeners(this.endListeners, touch);
 	},
 
 	callListeners(listenerList, touch) {
 		for (const [priority, listener] of listenerList) {
 			if (listener(touch)) {
-				break;
+				return true;
 			}
 		}
+		return false;
 	},
 
 	setMouse(event) {
@@ -92,7 +94,7 @@ Sunniesnow.TouchManager = {
 		const time = Sunniesnow.Music.convertTimeStamp(event.timeStamp);
 		const ctrlKey = navigator.platform.includes("Mac") ? event.metaKey : event.ctrlKey;
 		const touch = Sunniesnow.Touch.key(event.key, time, this.mousePageX, this.mousePageY, ctrlKey, event.shiftKey, event.altKey);
-		this.onStart(touch);
+		return this.onStart(touch);
 	},
 
 	keyUp(event) {
@@ -128,7 +130,7 @@ Sunniesnow.TouchManager = {
 		const time = Sunniesnow.Music.convertTimeStamp(event.timeStamp);
 		const ctrlKey = navigator.platform.includes("Mac") ? event.metaKey : event.ctrlKey;
 		const touch = Sunniesnow.Touch.mouseButton(event.button, time, this.mousePageX, this.mousePageY, ctrlKey, event.shiftKey, event.altKey);
-		this.onStart(touch);
+		return this.onStart(touch);
 	},
 
 	mouseMove(event) {
@@ -177,10 +179,12 @@ Sunniesnow.TouchManager = {
 		}
 		const time = Sunniesnow.Music.convertTimeStamp(event.timeStamp);
 		const ctrlKey = navigator.platform.includes("Mac") ? event.metaKey : event.ctrlKey;
+		let result = false;
 		for (const domTouch of event.changedTouches) {
 			const touch = Sunniesnow.Touch.domTouch(domTouch.identifier, time, domTouch.pageX, domTouch.pageY, ctrlKey, event.shiftKey, event.altKey);
-			this.onStart(touch);
+			result ||= this.onStart(touch);
 		}
+		return result;
 	},
 
 	touchMove(event) {
@@ -188,6 +192,7 @@ Sunniesnow.TouchManager = {
 			return;
 		}
 		const time = Sunniesnow.Music.convertTimeStamp(event.timeStamp);
+		let result = false;
 		for (const domTouch of event.changedTouches) {
 			const id = this.touchId(domTouch.identifier);
 			const touch = this.touches[id]
@@ -195,8 +200,9 @@ Sunniesnow.TouchManager = {
 				continue;
 			}
 			touch.move(time, domTouch.pageX, domTouch.pageY);
-			this.onMove(touch);
+			result ||= this.onMove(touch);
 		}
+		return result;
 	},
 
 	touchEnd(event) {
@@ -204,6 +210,7 @@ Sunniesnow.TouchManager = {
 			return;
 		}
 		const time = Sunniesnow.Music.convertTimeStamp(event.timeStamp);
+		let result = false;
 		for (const domTouch of event.changedTouches) {
 			const id = this.touchId(domTouch.identifier);
 			const touch = this.touches[id];
@@ -212,8 +219,9 @@ Sunniesnow.TouchManager = {
 			}
 			delete this.touches[id];
 			touch.move(time, domTouch.pageX, domTouch.pageY);
-			this.onEnd(touch);
+			result ||= this.onEnd(touch);
 		}
+		return result;
 	},
 
 	shouldIgnoreTouch(event) {
@@ -286,26 +294,19 @@ Sunniesnow.TouchManager = {
 		Sunniesnow.game.canvas.removeEventListener('touchend', this.touchEndListener);
 	},
 
-	preventKeyEventIfShould(event) {
-		if (!/^F\d+$/.test(event.key) && document.activeElement.contains(Sunniesnow.game.canvas)) {
-			event.preventDefault();
-		}
-	},
-
 	addDomKeyListeners() {
 		this.keyDownListener = event => {
-			if (this.mouseOut()) {
-				return;
-			}
-			this.preventKeyEventIfShould(event);
 			if (['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) {
 				return;
 			}
-			this.keyDown(event);
+			if (this.keyDown(event)) {
+				event.preventDefault();
+			}
 		}
 		this.keyUpListener = event => {
-			this.preventKeyEventIfShould(event);
-			this.keyUp(event);
+			if (this.keyUp(event)) {
+				event.preventDefault();
+			}
 		}
 		Sunniesnow.game.document.addEventListener('keydown', this.keyDownListener);
 		Sunniesnow.game.document.addEventListener('keyup', this.keyUpListener);

js/interaction/ProgressControl.js
@@ -73,7 +73,7 @@ Sunniesnow.ProgressControl = {
 
 	addDraggingListeners() {
 		this.dragStartListener = touch => {
-			if (touch.start().canvasY < Sunniesnow.Config.HEIGHT - this.touchHeight) {
+			if (!Sunniesnow.Utils.between(touch.start().canvasY, Sunniesnow.Config.HEIGHT - this.touchHeight, Sunniesnow.Config.HEIGHT)) {
 				return false;
 			}
 			this.draggingTouches.push(touch);

commit c471ab4c8ad1e72b44a315444d556f07319f4f86

fix bad touch input bug introduced by 36757c0

js/touch/TouchManager.js
@@ -182,7 +182,7 @@ Sunniesnow.TouchManager = {
 		let result = false;
 		for (const domTouch of event.changedTouches) {
 			const touch = Sunniesnow.Touch.domTouch(domTouch.identifier, time, domTouch.pageX, domTouch.pageY, ctrlKey, event.shiftKey, event.altKey);
-			result ||= this.onStart(touch);
+			result = this.onStart(touch) || result;
 		}
 		return result;
 	},
@@ -200,7 +200,7 @@ Sunniesnow.TouchManager = {
 				continue;
 			}
 			touch.move(time, domTouch.pageX, domTouch.pageY);
-			result ||= this.onMove(touch);
+			result = this.onMove(touch) || result;
 		}
 		return result;
 	},
@@ -219,7 +219,7 @@ Sunniesnow.TouchManager = {
 			}
 			delete this.touches[id];
 			touch.move(time, domTouch.pageX, domTouch.pageY);
-			result ||= this.onEnd(touch);
+			result = this.onEnd(touch) || result;
 		}
 		return result;
 	},